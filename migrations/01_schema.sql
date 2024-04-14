5810390ac1672acdf93457158a307bef5059d77e
Jeremiah Chua <jeremiahjchua@hotmail.com>
2024-04-14 13:25:10 -0700

    Seed LightBnB

--- migrations/01_schema.sql
+++ migrations/01_schema.sql
@@ -3,6 +3,7 @@ DROP TABLE IF EXISTS properties CASCADE;
 DROP TABLE IF EXISTS reservations CASCADE;
 DROP TABLE IF EXISTS property_reviews CASCADE;
 
+
 CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -10,9 +11,11 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL
 );
 
+
 CREATE TABLE properties (
   id SERIAL PRIMARY KEY NOT NULL,
   owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+
 
   title VARCHAR(255) NOT NULL,
   description TEXT,
@@ -23,14 +26,17 @@ CREATE TABLE properties (
   number_of_bathrooms INTEGER  NOT NULL DEFAULT 0,
   number_of_bedrooms INTEGER  NOT NULL DEFAULT 0,
 
+
   country VARCHAR(255) NOT NULL,
   street VARCHAR(255) NOT NULL,
   city VARCHAR(255) NOT NULL,
   province VARCHAR(255) NOT NULL,
   post_code VARCHAR(255) NOT NULL,
 
+
   active BOOLEAN NOT NULL DEFAULT TRUE
 );
+
 
 CREATE TABLE reservations (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -39,6 +45,7 @@ CREATE TABLE reservations (
   property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
   guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
 );
+
 
 CREATE TABLE property_reviews (
   id SERIAL PRIMARY KEY NOT NULL,
